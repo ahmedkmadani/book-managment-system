d326110d1690fabc041c0f50ef9b35fdbb4f46e8
Ahmed K Madani <ahmedkamal543@gmail.com>
2022-03-24 15:39:40 +0300

    init commit

---
+++ cmd/main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/ahmedkmadani/book-system/pkg/routes"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
